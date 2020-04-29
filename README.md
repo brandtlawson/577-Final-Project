e9de3150c4592b8b6d4bb9e0dc67d2d0b2ad345b
brandtlawson <43549359+brandtlawson@users.noreply.github.com>
2020-04-29 00:12:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 577-Final-Project
+ 577-Final-Project
---
+++ test_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 19:34:34 2020
+
+@author: brandtlawson
+"""
+def test_function(str):
+        print(str)
+        return str
