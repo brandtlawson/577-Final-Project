17cd44ba9a482c939937f07c326dd8f67b51bb89
brandtlawson <43549359+brandtlawson@users.noreply.github.com>
2020-05-01 00:00:28 -0500

    commit

---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+"The work I submitted represents my work and my work alone.  I abode by the academic integrity policy and I am aware of the consequences associated with engaging in academic misconduct. "
--- test_function.py
+++ test_function.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 28 19:34:34 2020
-
-@author: brandtlawson
-"""
-def test_function(str):
-        print(str)
-        return str
+def test_my_function():
+    print("noodles")
+    return "noodles"
+    
